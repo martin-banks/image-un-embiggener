1dd1a670ba67b1c8e49ac2365b3b25391f487e73
Martin Banks <anotherbanksy@gmail.com>
2019-03-06 21:22:13 +1100

    minimise demo rules for faster tsting

--- src/image-models/demo.js
+++ src/image-models/demo.js
@@ -1,19 +1,19 @@
 const crops = [
-  {
-    context: 'mobile',
-    files: {
-      jpg: true,
-      png: true,
-    },
-    width: 320,
-    suffix: '@1x',
-    blur: 0,
-    dither: 100,
-    sharpen: 0,
-    quality: {
-      jpg: 30,
-    },
-  },
+  // {
+  //   context: 'mobile',
+  //   files: {
+  //     jpg: true,
+  //     png: true,
+  //   },
+  //   width: 320,
+  //   suffix: '@1x',
+  //   blur: 0,
+  //   dither: 100,
+  //   sharpen: 0,
+  //   quality: {
+  //     jpg: 30,
+  //   },
+  // },
   {
     context: 'mobile',
     files: {
@@ -30,69 +30,69 @@ const crops = [
     },
   },
 
-  {
-    context: 'desktop',
-    files: {
-      jpg: true,
-      png: true,
-    },
-    width: 650,
-    suffix: '@1x',
-    blur: 0,
-    dither: 100,
-    sharpen: 0,
-    quality: {
-      jpg: 30,
-    },
-  },
-  {
-    context: 'desktop',
-    files: {
-      jpg: true,
-      png: true,
-    },
-    width: 1300,
-    suffix: '@2x',
-    blur: 0,
-    dither: 100,
-    sharpen: 0,
-    quality: {
-      jpg: 30,
-    },
-  },
+  // {
+  //   context: 'desktop',
+  //   files: {
+  //     jpg: true,
+  //     png: true,
+  //   },
+  //   width: 650,
+  //   suffix: '@1x',
+  //   blur: 0,
+  //   dither: 100,
+  //   sharpen: 0,
+  //   quality: {
+  //     jpg: 30,
+  //   },
+  // },
+  // {
+  //   context: 'desktop',
+  //   files: {
+  //     jpg: true,
+  //     png: true,
+  //   },
+  //   width: 1300,
+  //   suffix: '@2x',
+  //   blur: 0,
+  //   dither: 100,
+  //   sharpen: 0,
+  //   quality: {
+  //     jpg: 30,
+  //   },
+  // },
 
-  {
-    context: 'thumb',
-    files: {
-      jpg: true,
-      png: true,
-    },
-    width: 20,
-    suffix: '@blur',
-    dither: 100,
-    blur: 3,
-    dither: 100,
-    sharpen: 0,
-    quality: {
-      jpg: 20,
-    },
-  },
+  // {
+  //   context: 'thumb',
+  //   files: {
+  //     jpg: true,
+  //     png: true,
+  //   },
+  //   width: 20,
+  //   suffix: '@blur',
+  //   dither: 100,
+  //   blur: 3,
+  //   dither: 100,
+  //   sharpen: 0,
+  //   quality: {
+  //     jpg: 20,
+  //   },
+  // },
 
-  {
-    context: 'thumb',
-    files: {
-      jpg: true,
-      png: true,
-    },
-    width: 50,
-    suffix: '@50',
-    blur: 0,
-    dither: 100,
-    sharpen: 0,
-    quality: {
-      jpg: 50,
-    },
-  },
+  // {
+  //   context: 'thumb',
+  //   files: {
+  //     jpg: true,
+  //     png: true,
+  //   },
+  //   width: 50,
+  //   suffix: '@50',
+  //   blur: 0,
+  //   dither: 100,
+  //   sharpen: 0,
+  //   quality: {
+  //     jpg: 50,
+  //   },
+  // },
 ]
 
 const report = {
