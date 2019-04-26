0237cd44be36ba224c2daff320b774ed2da151e5
Martin Banks <anotherbanksy@gmail.com>
2019-04-26 23:55:32 +1000

    basic nav style

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
-class Nav extends Component {
+const Nav = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 150px;
+  height: 100vh;
+  background: rgba(0,0,0, 0.2);
+  padding: 8px;
+  padding-top: 50px;
+  border-right: solid 1px rgba(255,255,255, 0.2)
+`
+
+const LinkWrapper = styled.ul`
+  padding: 0;
+  margin: 0;
+`
+
+const PageLink = styled.li`
+  list-style: none;
+  padding: 8px;
+  border-radius: 4px;
+  &:hover {
+    background: rgba(0,0,0, 0.2);
+  }
+`
+
+export default class extends Component {
   constructor (props) {
     super(props)
   }
 
   render () {
     return (
-      <nav>
-        <ul>
-          <li onClick={ this.props.setPage.bind(null, 'optimiser')}>Optimiser</li>
-          <li onClick={ this.props.setPage.bind(null, 'tips')}>Tips</li>
-        </ul>
-      </nav>
+      <Nav>
+        <LinkWrapper>
+          <PageLink onClick={ this.props.setPage.bind(null, 'optimiser')}>Optimiser</PageLink>
+          <PageLink onClick={ this.props.setPage.bind(null, 'tips')}>Tips</PageLink>
+        </LinkWrapper>
+      </Nav>
     )
   }
 }
 
-export default Nav
+
