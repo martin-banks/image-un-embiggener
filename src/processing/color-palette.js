795fbcf47f7e57a97ea13eca87461659cae4a1ad
Martin Banks <anotherbanksy@gmail.com>
2019-05-04 17:33:47 +1000

    adds rgb value cleaner

--- src/processing/color-palette.js
+++ src/processing/color-palette.js
@@ -1,12 +1,28 @@
 const vibrant = require('node-vibrant')
 
+function makeRGBInt (obj) {
+  return Object.keys(obj).reduce((prev, key) => {
+    const output = prev
+    let update = obj[key]
+    if (obj[key] && obj[key]._rgb) {
+      const fixedRGB = obj[key]._rgb.map(v => Math.round(v))
+      update._rgb = fixedRGB
+      output[key] = update
+    }
+    return output
+  }, {})
+}
 
-function palette () {
+function palette (imagePath) {
   return new Promise((resolve, reject) => {
     vibrant
       .from(imagePath)
       .getPalette((err, pal) => {
-        if (err) return reject(err)
+        console.log({ pal })
+        if (err) {
+          console.log('\n--------\nERROR GETTING COLOR PALETTE\n-----------\n', err)
+          return reject(err)
+        }
         const colors = makeRGBInt(pal)
         // low color pictures do not always generate a vibrant color pallette
         // if (!colors.Vibrant) colors.Vibrant = { _rgb: [0, 0, 0] }
